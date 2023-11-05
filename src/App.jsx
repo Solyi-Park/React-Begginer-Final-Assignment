a47033703c431d95ae9b89471f1f134f6c4aa2c1
Solyi Park <itsoholy21@gmail.com>
2023-11-06 00:21:13 +0900

    완료, 삭제, 취소버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,65 @@ function App() {
   const [cards, setCards] = useState([
     { id: 1, title: "Sample task", content: "Sample detailed content" },
   ]);
+  const [done, setDone] = useState([
+    { id: 1, title: " task", content: "Sample detailed content" },
+  ]);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const handleChangeTitle = (e) => setTitle(e.target.value);
   const handleChangeContent = (e) => setContent(e.target.value);
-  const handleAddButtonClick = () => {
+
+  // 추가하기 버튼 클릭
+  const handleAddBtnClick = () => {
     const newCard = {
       id: cards.length + 1,
       title,
       content,
     };
-    const cardUpdate = [...cards, newCard];
-    setCards(cardUpdate);
-    console.log(cardUpdate);
-    alert("Added new task!");
+    if (title == "") {
+      alert("제목을 입력해주세요. ");
+    } else {
+      const updateCards = [...cards, newCard];
+      setCards(updateCards);
+      console.log(updateCards);
+      // alert("Added new task!");
+    }
+  };
+
+  //삭제하기 버튼 클릭
+  const handleDeleteBtn = (id) => {
+    const updateCards = cards.filter((card) => card.id !== id);
+    setCards(updateCards);
+  };
+
+  // 완료 버튼 클릭
+  const handleCompleteBtn = (item) => {
+    // 클릭하면 cards에서 해당 카드를 지운다
+    const updateCards = cards.filter((card) => card.id !== item.id);
+    setCards(updateCards);
+    // done section에 카드를 붙인다.
+    const isDone = {
+      id: done.length + 1,
+      title: item.title,
+      content: item.content,
+    };
+      const moveToDone = [...done, isDone];
+      setDone(moveToDone);
+  };
+
+  // 취소 버튼 클릭
+  const handleCancelBtn = (clickedItem) => {
+    // 클릭하면 cards에서 해당 카드를 지운다
+    const updateDoneCards = done.filter((doneItem) => doneItem.id !== clickedItem.id);
+    setDone(updateDoneCards);
+    // done section에 카드를 붙인다.
+    const notDone = {
+      id: done.length + 1,
+      title: clickedItem.title,
+      content: clickedItem.content,
+    };
+      const moveToWorking = [...cards, notDone];
+      setCards(moveToWorking);
   };
 
   return (
@@ -44,7 +89,7 @@ function App() {
             placeholder="Enter detailed content."
           />
         </div>
-        <button onClick={handleAddButtonClick} className="addButton">
+        <button onClick={handleAddBtnClick} className="addButton">
           추가하기
         </button>
       </div>
@@ -61,8 +106,18 @@ function App() {
                   <p>{item.content}</p>
                 </div>
                 <div>
-                  <button className="cardButton deleteButton">삭제하기</button>
-                  <button className="cardButton completeButton">완료</button>
+                  <button
+                    onClick={() => handleDeleteBtn(item.id)}
+                    className="cardButton deleteButton"
+                  >
+                    삭제하기
+                  </button>
+                  <button
+                    onClick={() => handleCompleteBtn(item)}
+                    className="cardButton completeButton"
+                  >
+                    완료
+                  </button>
                 </div>
               </div>
             );
@@ -74,16 +129,30 @@ function App() {
           <h2>Done..! 🎉</h2>
         </div>
         <div className="cardContainer">
-          <div className="card">
-          <div>
-            <h2>리액트 공부하기</h2>
-            <p>리액트 기초를 공부해봅시다.</p>
-          </div>
-          <div>
-            <button className="cardButton deleteButton">삭제하기</button>
-            <button className="cardButton cancelButton">취소</button>
-          </div>
-        </div>
+          {done.map((item) => {
+            return (
+              <div className="card">
+                <div>
+                  <h2>{item.title}</h2>
+                  <p>{item.content}</p>
+                </div>
+                <div>
+                  <button
+                    onClick={() => handleDeleteBtn(item.id)}
+                    className="cardButton deleteButton"
+                  >
+                    삭제하기
+                  </button>
+                  <button
+                    onClick={()=> handleCancelBtn(item)}
+                    className="cardButton cancelButton"
+                  >
+                    취소
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
